fdc73fcfbe3f1780956dc7c8c6495d5d22d5ea3d
zaky-naufal <mzakynaufalbasuki@gmail.com>
2026-04-26 02:49:35 +0700

    Tugas 2

---
+++ resources/views/meeting2.blade.php
@@ -0,0 +1,121 @@
+<html>
+
+<head>
+    <title>RoboDog Kendali Jarak Jauh, Inovasi ITS bersama Indosat, Nokia, dan NVIDIA</title>
+    <style>
+        body {
+            background-color: #e6f2ff;
+            margin-top: 50px;
+            margin-bottom: 100px;
+            margin-right: 80px;
+            margin-left: 80px;
+        }
+
+        p {
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+        }
+
+        #bikinjaditengah {
+            text-align: center;
+            font-size: xx-small;
+        }
+
+        .bikinjaditengah2 {
+            text-align: center;
+            font-size: xx-small;
+        }
+
+        .judulberita {
+            text-align: center;
+            font-size: large;
+            color: rgb(55, 95,223);
+        }
+
+        .judulgambar {
+            text-align: center;
+            font-size: x-small;
+        }
+
+        .footerbold {
+            font-weight: bold;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+        }
+
+        a:link{
+            text-decoration: none;
+            color: rgb(55, 95,223);
+        }
+        a:hover{
+            text-decoration: none;
+            color: rgb(55, 95,223);
+        }
+        a:visited{
+            text-decoration: none;
+            color: rgb(55, 95,223);
+        }
+
+        .formatgambar{
+            border-radius: 15px;
+            box-shadow: 0px 4px 10px 2px rgba(0, 0, 0, 0.2);
+        }
+    </style>
+</head>
+
+<body>
+    <p class="judulberita">RoboDog Kendali Jarak Jauh, Inovasi ITS bersama Indosat, Nokia, dan NVIDIA</p>
+    <p id="bikinjaditengah">Published on<br>
+        Minggu, 1 Maret 2026</p>
+    <p ><center><img class="formatgambar"src="https://www.its.ac.id/news/wp-content/uploads/sites/2/2026/03/kerjasama-robodog-2-1024x682.jpeg" width="600px"></center></p>
+    <p class="judulgambar">Demonstrasi RoboDog untuk mengidentifikasi kurir pengirim paket dengan kontrol jarak jauh</p><p>Surabaya, ITS News — <a href="https://www.its.ac.id/" target="_blank"> Institut Teknologi Sepuluh Nopember (ITS)</a> kembali menegaskan kapasitasnya dalam
+        pengembangan robotika dan artificial intelligence (AI). Kali ini, ITS bersama Indosat Ooredoo Hutchison (IOH),
+        Nokia, NVIDIA memperkenalkan inovasi RoboDog, robot anjing otonom dengan kendali ribuan kilometer di AI-Ran
+        Research Center, Surabaya, Sabtu (28/2).</p>
+    <p>Sinergi empat institusi menjadi kunci terwujudnya inovasi ini. IOH memimpin pengembangan Artificial Intelligence
+        Radio Access Network (AI-RAN), sementara Nokia menghadirkan infrastruktur jaringan 5G sebagai tulang punggung
+        konektivitas. Di sisi lain, NVIDIA mendukung komputasi berbasis Graphics Processing Unit dan ITS
+        mengintegrasikannya ke dalam fisik robotika yang siap dioperasikan.</p>
+    <p>Perwakilan Pengembang RoboDog ITS Yuke Brilliant Hestiavin memaparkan bahwa RoboDog dirancang mampu menerima
+        perintah suara manusia dan memahami konteks instruksi. Tak hanya itu, sensor kamera dan LiDAR RoboDog
+        memungkinkan robot untuk membaca kondisi sekitar secara real-time. “Kemampuan ini dapat dikendalikan dalam jarak
+        ribuan kilometer,” ungkap Yuke.</p>
+    <p>Dalam skenario demonstrasi, RoboDog di Surabaya, Indonesia dapat menerima perintah suara dari pengguna di
+        Barcelona, Spanyol untuk memeriksa kondisi suatu lokasi, mendeteksi potensi bahaya, hingga memastikan tidak ada
+        aktivitas mencurigakan. Mahasiswa Departemen Teknik Komputer ITS tersebut menilai bahwa kemampuan robot untuk
+        memahami situasi membuatnya aplikatif untuk industri yang berisiko tinggi bagi manusia.</p>
+    <p><center><img class="formatgambar" src="https://www.its.ac.id/news/wp-content/uploads/sites/2/2026/03/kerjasama-robodog-3-1024x712.jpeg" width="600px"></center></p>
+    <p class="judulgambar">Perwakilan Pengembang RoboDog ITS Yuke Brilliant Hestiavin saat mempersiapkan RoboDog untuk demonstrasi
+        pengendalian jarak jauh di Kantor Indosat Surabaya
+    <p>Di balik kecanggihan itu, VP Head of Technology Strategy & Partnership Management IOH Irwan Radius menyebut
+        AI-RAN sebagai fondasi sistem. AI-RAN menghubungkan perintah dari jarak jauh dengan aksi robot melalui jaringan
+        5G. “Teknologi ini memungkinkan sinkronisasi antara pusat pemantauan di Spanyol dan robot di Surabaya dengan
+        jeda sangat rendah,” klaim Irwan.</p>
+    <p>Melalui pendekatan ini, lanjut Irwan, kecerdasan buatan tidak lagi diproses di server terpusat yang jauh,
+        melainkan diintegrasikan pada infrastruktur jaringan 5G. Jaringan yang semula hanya berperan sebagai pengirim
+        data, kini dapat menjadi pusat pemrosesan komputasi berbasis AI. “Arsitektur ini memungkinkan latensi
+        ultra-rendah yang dibutuhkan untuk pengembangan AI berbentuk fisik seperti robotika dan drone otonom,” paparnya.
+    </p>
+    <p><center><img class="formatgambar" src="https://www.its.ac.id/news/wp-content/uploads/sites/2/2026/03/kerjasama-robodog-1-1024x686.jpeg" width="600px"></center></p>
+    <p class="judulgambar">Perwakilan Pengembang RoboDog menyimulasikan tampilan user interface RoboDog ketika robot dikendalikan dari
+        Barcelona, Spanyol
+    <p>Implementasi teknologi tersebut akan dipamerkan dalam ajang Mobile World Congress 2026 yang digelar di Barcelona,
+        Spanyol, pada Maret 2026 mendatang. Irwan menilai partisipasi ini menjadi penegasan bahwa Indonesia tidak
+        sekadar menjadi pasar bagi teknologi AI, tetapi juga mampu menciptakannya. “Semoga melalui forum internasional
+        ini, inovasi anak bangsa semakin mendapat ruang dan peluang kolaborasi di kancah global,” tutupnya penuh harap.
+    </p>
+    <p>Partisipasi ITS mencerminkan kapasitas sumber daya manusia dalam merancang inovasi robotika dan kecerdasan buatan
+        yang relevan bagi industri masa depan. Langkah ini sejalan dengan Sustainable Development Goals (SDGs),
+        khususnya poin 4 tentang Pendidikan Berkualitas serta poin 9 tentang Industri, Inovasi, dan Infrastruktur.
+        Selain itu, juga mendukung poin 17 mengenai Kemitraan untuk Mencapai Tujuan. (HUMAS ITS)</p>
+    <p>
+        <div class="footerbold bikinjaditengah2" >
+        Reporter: Naurah Fitri</div>
+    </p>
+    <p>
+        <span>Media Sosial</span><br>
+    @its_campus<br>
+    @its_campus<br>
+    @itseurekatv<br>
+    @its_campus</p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
 Route::get('/halo', function () {
 	return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
 });
@@ -21,5 +19,34 @@ Route::get('pertemuan5', function () {
 	return view('meeting5');
 
 });
+
+Route::get('pertemuan4', function () {
+	return view('meeting4');
+
+});
+
+Route::get('tugasBS4', function () {
+	return view('tugas4');
+
+});
+
+
+Route::get('pertemuan3', function () {
+	return view('meeting3');
+});
+
+Route::get('pertemuan2', function () {
+	return view('meeting2');
+
+});
+
+Route::get('pertemuan1', function () {
+	return view('meeting1');
+
+});
+
+
+
+
 
 Route::get('dosen', [DosenController::class, 'index']);
